f33dcdf0d511e232332379a96ae6323e02ccf6a3
Alexei Samokvalov <alexei@sam701.com>
2024-09-25 16:52:19 +0200

    Add chain_id label

--- balance.go
+++ balance.go
@@ -12,25 +12,34 @@ import (
 )
 
 const (
-	PROM_LABEL_CHAIN_NAME = "chain_name"
-	PROM_LABEL_ADDRESS    = "addr"
+	PROM_LABEL_NETWORK_NAME = "network_name"
+	PROM_LABEL_CHAIN_ID     = "chain_id"
+	PROM_LABEL_ADDRESS      = "addr"
 )
 
 var (
 	parentChainBalance = promauto.NewGaugeVec(prometheus.GaugeOpts{
 		Name: "validator_balance",
-	}, []string{PROM_LABEL_CHAIN_NAME, PROM_LABEL_ADDRESS})
+	}, []string{PROM_LABEL_NETWORK_NAME, PROM_LABEL_CHAIN_ID, PROM_LABEL_ADDRESS})
 )
 
 func runBalanceChecker(client *ethclient.Client, addressToCheck common.Address, chainName string, interval time.Duration) {
 	logger := slog.With("chain", chainName)
+	chainId, err := client.ChainID(context.Background())
+	var chainIdStr string
+	if err == nil {
+		chainIdStr = chainId.String()
+	} else {
+		logger.Error("failed to get chain ID", "error", err)
+	}
 	for {
 		balance, err := client.BalanceAt(context.Background(), addressToCheck, nil)
 		if err != nil {
 			logger.Error("failed to get balance", "error", err)
 		} else {
 			logger.Debug("got balance", "balance", balance)
-			parentChainBalance.WithLabelValues(chainName, addressToCheck.Hex()).Set(weiToPromEth(balance))
+			val, _ := balance.Float64()
+			parentChainBalance.WithLabelValues(chainName, chainIdStr, addressToCheck.Hex()).Set(val)
 		}
 
 		logger.Debug("sleeping", "duration", interval)
