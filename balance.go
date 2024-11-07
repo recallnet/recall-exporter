2dc655a168da4782a6dfaa9e56ff444df478f916
Alexei Samokvalov <alexei@sam701.com>
2024-11-07 12:20:30 +0100

    Add job to expose HOKU balance on parent chain

--- balance.go
+++ balance.go
@@ -14,17 +14,44 @@ const (
 	PROM_LABEL_NETWORK_NAME = "network_name"
 	PROM_LABEL_CHAIN_ID     = "chain_id"
 	PROM_LABEL_ADDRESS      = "addr"
+	PROM_LABEL_KIND         = "kind"
 )
 
 func newBalanceCheckerJob(ep *Endpoint, addressToCheck common.Address) JobFunc {
 	gaugeBalance := promauto.NewGauge(prometheus.GaugeOpts{
-		Namespace:   PROM_NAMESPACE_HOKU,
-		Name:        "validator_balance",
-		ConstLabels: ep.ConstLabels(PROM_LABEL_ADDRESS, addressToCheck.Hex()),
+		Namespace: PROM_NAMESPACE_HOKU,
+		Name:      "validator_balance",
+		ConstLabels: ep.ConstLabels(
+			PROM_LABEL_ADDRESS, addressToCheck.Hex(),
+			PROM_LABEL_KIND, "native",
+		),
 	})
 
 	return func(logger *slog.Logger) error {
 		balance, err := ep.Client.BalanceAt(context.Background(), addressToCheck, nil)
+		if err != nil {
+			return fmt.Errorf("failed to get balance: %w", err)
+		} else {
+			logger.Debug("got balance", "balance", balance)
+			val, _ := balance.Float64()
+			gaugeBalance.Set(val)
+		}
+		return nil
+	}
+}
+
+func newErc20TokenBalanceCheckerJob(ep *ParentChainEndpoint, addressToCheck common.Address) JobFunc {
+	gaugeBalance := promauto.NewGauge(prometheus.GaugeOpts{
+		Namespace: PROM_NAMESPACE_HOKU,
+		Name:      "validator_balance",
+		ConstLabels: ep.ConstLabels(
+			PROM_LABEL_ADDRESS, addressToCheck.Hex(),
+			PROM_LABEL_KIND, "erc20",
+		),
+	})
+
+	return func(logger *slog.Logger) error {
+		balance, err := ep.SupplySourceCaller.BalanceOf(nil, addressToCheck)
 		if err != nil {
 			return fmt.Errorf("failed to get balance: %w", err)
 		} else {
